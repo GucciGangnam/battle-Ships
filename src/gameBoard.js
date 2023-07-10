5ecc23d417e2272ef99bea4f69a1ad7d7c87cb0f
Gucci Gangnam <diviner02hive@icloud.com>
2023-07-10 17:22:11 +0700

    all gameboard methods added and tested. NEXT - create player class and interface

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -44,10 +44,46 @@ class GameBoard {
         return response;
     }
 
+    placeShip(ship, x, y) {
+        if (this.testIfShipIsLegal(ship, x, y) == 0) {
+            if (ship.direction === 'horizontal') {
+                for (let i = 0; i < ship.length; i++) {
+                    this.board[x][y + i] = ship;
+                }
+            } else {
+                for (let i = 0; i < ship.length; i++) {
+                    this.board[x + i][y] = ship;
+                }
+            }
+            this.ships.push(ship);
+        } else {
+            return "error: did not pass legal test";
+        }
+    }
 
+    receiveAttack(x, y) {
+        if (this.board[x][y] === "empty") {
+            this.misses.push([x, y]);
+            return "miss";
+        } else {
+            this.board[x][y].hit();
+            if (this.board[x][y].sunk === true) {
+                return "sunk";
+            } else {
+                return "hit";
+            }
+        }
+    }
 
-
-
+    allShipsSunk() {
+        let sunk = true;
+        this.ships.forEach(ship => {
+            if (ship.sunk === false) {
+                sunk = false;
+            }
+        });
+        return sunk;
+    }
 
 }
 
