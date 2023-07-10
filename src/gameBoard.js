26f8ce777ff00794b6bd4894214c32905a66ce02
Gucci Gangnam <diviner02hive@icloud.com>
2023-07-10 16:41:35 +0700

    gameboard class created with constructors and methods to creatre the board and test if the placement of ship on chosen coords is legal, NEXT - create place legal ship method and build tests

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -13,12 +13,39 @@ class GameBoard {
         for (let i = 0; i < 10; i++) {
             this.board.push([]);
             for (let j = 0; j < 10; j++) {
-                this.board[i].push(null);
+                this.board[i].push("empty");
             }
         }
     }
 
-    
+    testIfShipIsLegal(ship, x, y) { 
+        let response = 0;
+        if (ship.direction === 'horizontal') {
+            for (let i = 0; i < ship.length; i++) {
+                if (this.board[x] && this.board[x][y + i] == "empty") {
+                    response += 0;
+                } else {
+                    response -= 1;
+                    return response;
+                }
+            } 
+        } else {
+            for (let i = 0; i < ship.length; i++) {
+
+                if (this.board[x + i] && this.board[x + i][y] == "empty") {
+
+                    response += 0;
+                } else {
+                    response -= 1;
+                    return response;
+                }
+            } 
+        }
+        return response;
+    }
+
+
+
 
 
 
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -1,4 +1,5 @@
 //imports 
+import Ship from './ship';
 import GameBoard from './gameBoard';
 
 //tests
@@ -7,5 +8,25 @@ test('createBoard creates a 10x10 board', () => {
     gameBoard.createBoard();
     expect(gameBoard.board.length).toBe(10);
     expect(gameBoard.board[0].length).toBe(10);
+}
+);
+
+
+test ('testIfShipIsLegal returns true if ship can be placed', () => {
+    const gameBoard = new GameBoard();
+    gameBoard.createBoard();
+    const ship = new Ship(9);
+    ship.changeDirection();
+    const ship2 = new Ship(9);
+
+    expect(gameBoard.testIfShipIsLegal(ship, 0,0)).toBe(0);
+    expect(gameBoard.testIfShipIsLegal(ship2, 0,0)).toBe(0);
+    expect(gameBoard.testIfShipIsLegal(ship2, 4,0)).toBe(0);
+    expect(gameBoard.testIfShipIsLegal(ship2, 4,4)).toBe(-1);
+    expect(gameBoard.testIfShipIsLegal(ship2, 0,3)).toBe(-1);
+    expect(gameBoard.testIfShipIsLegal(ship, 3,0)).toBe(-1);
+
+
+
 }
 );
